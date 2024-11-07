a78fc5aba1ad823a52b3c4686b70a75a080079de
Secuko <artem.df34@gmail.com>
2024-11-07 19:01:39 +0300

    third-commit

--- src/components/MainPage/Instructions.vue
+++ src/components/MainPage/Instructions.vue
@@ -5,24 +5,30 @@ export default {
         const H2_TEXT_1 = "Готовы начать?"
         const SMALL_TEXT_1 = "Независимо от того, являетесь ли вы разработчиком, мерчантом, конечным пользователем или покупателем окон, мы приветствуем вас в сообществе BTCA."
         const H2_TEXT_2 = "Никогда не пропускайте обновления"
-        const SMALL_TEXT_2 = "Подпишитесь на информационный бюллетень и будьте первым, кто получит новейшие оповещения, объявления и обновления о разработках BTCA."
+        const SMALL_TEXT_2 = "Подпишитесь на информационный бюллетень\nи будьте первым, кто получит новейшие оповещения, объявления\nи обновления о разработках BTCA."
         const BUTTON_TEXT = "Подписаться"
         const EMAIL_PLACEHOLDER = "Ваш e-mail"
         const INSTRUCTIONS = [
             {
                 step_index: "1",
                 step: "шаг",
-                text: "Скачайте\nприложение"
+                text: "Скачайте\nприложение",
+                src: new URL('../../assets/images/instruction1.png', import.meta.url).href,
+                alt: 'image',
             },
             {
                 step_index: "2",
                 step: "шаг",
-                text: "Регистрируйтесь"
+                text: "Регистрируйтесь",
+                src: new URL('../../assets/images/instruction2.png', import.meta.url).href,
+                alt: 'image',
             },
             {
                 step_index: "3",
                 step: "шаг",
-                text: "Пользуйтесь"
+                text: "Пользуйтесь",
+                src: new URL('../../assets/images/instruction3.png', import.meta.url).href,
+                alt: 'image',
             },
         ]
 
@@ -53,7 +59,8 @@ export default {
             </div>
         </div>
         <div class="instructions">
-            <div class="instruction" v-for="(item, index) in INSTRUCTIONS" :key="index">
+            <div class="instruction" v-for="(item, index) in INSTRUCTIONS" :key="index"
+                :style="{ backgroundImage: `url(${item.src})`, alt: `${item.alt}` }">
                 <div class="instruction__header">
                     <span class="step_index"> {{ item.step_index }}</span>
                     <span class="step">{{ item.step }}</span>
@@ -67,14 +74,19 @@ export default {
                 </div>
             </div>
         </div>
-        <form class="subsribe-field">
-            <div class="subsribe-field__h2">
-
+        <form class="subsribe-banner">
+            <div class="subsribe-banner__h2">
+                <h2>{{ H2_TEXT_2 }}</h2>
             </div>
-            <div class="subsribe-field__text">
-
+            <div class="subsribe-banner__text">
+                <p>{{ SMALL_TEXT_2 }}</p>
             </div>
-            <input type="email" name="email" id="email" required>
+            <div class="input-field">
+                <input type="email" name="email" id="email" required :placeholder="{EMAIL_PLACEHOLDER}">
+                <button class="subscribe-button">
+                    <span>{{ BUTTON_TEXT }}</span>
+                </button>
+            </div>
         </form>
     </div>
 </template>
@@ -92,11 +104,6 @@ export default {
     padding-top: 12.8rem;
 }
 
-.instructions {
-    display: flex;
-    flex-direction: row;
-}
-
 .text-block {
     display: flex;
     flex-direction: column;
@@ -106,7 +113,7 @@ export default {
     max-width: 54.3rem;
 }
 
-.text-block__h2{
+.text-block__h2 {
     @include text(h2);
     background: linear-gradient(91.92deg, $white 29.24%, rgba(176, 250, 255, 0) 131.64%);
     color: transparent;
@@ -114,59 +121,105 @@ export default {
     -webkit-background-clip: text;
 }
 
-.text-block__text{
+.text-block__text {
     @include text(p);
     color: $white;
     text-align: center;
 }
 
-.instructions{
+.instructions {
     margin-top: 4.9rem;
     display: flex;
     flex-direction: row;
     gap: 2.4rem;
 }
 
-.instruction{
+.instruction {
     height: 25.7rem;
     width: 36.5rem;
     border-radius: 0.8rem;
     background: $sliderBlack;
     padding-top: 3rem;
     padding-left: 2.8rem;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
 }
 
 .instruction__header {
     display: flex;
-    align-items: center; /* Выравнивание элементов по вертикали по центру */
-  }
+    align-items: center;
+    /* Выравнивание элементов по вертикали по центру */
+}
 
-.step_index{
+.step_index {
     @include text(caption);
     color: $white;
     margin-left: 0.5rem;
     margin-right: 0.7rem;
 }
 
-.step{
+.step {
     @include text(p);
-   color: rgba($color: $white, $alpha: 0.8);
-   width: 3.3rem;
-   margin-right: 2.8rem;
+    color: rgba($color: $white, $alpha: 0.8);
+    width: 3.3rem;
+    margin-right: 2.8rem;
 }
 
-.separated-line{
+.separated-line {
     flex-grow: 1;
     height: 0.1rem;
     background: rgba($color: $white, $alpha: 0.1);
 }
 
-.instruction__h3{
+.instruction__h3 {
     @include text(h3);
     color: $white;
     white-space: pre-line;
 }
 
+.subsribe-banner{
+    padding-top: 7.5rem;
+    padding-left: 5.4rem;
+    margin-top: 3.2rem;
+    width: 114rem;
+    height: 40.2rem;
+    border-radius: 2.8rem;
+    background: url('../../assets/images/banner.png');
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+}
+
+.subsribe-banner__h2{
+    @include text(h2);
+    background: linear-gradient(91.92deg, $white 29.24%, rgba(176, 250, 255, 0) 131.64%);
+    color: transparent;
+    background-clip: text;
+    -webkit-background-clip: text;
+    margin-bottom: 1.6rem;
+}
+
+.subsribe-banner__text{
+    @include text(p);
+    color: rgba($color: $white, $alpha: 0.8);
+    white-space: pre-line;
+    margin-bottom: 3.2rem;
+}
+
+
+.subscribe-button{
+    box-shadow: 0px 4px 16px 0px #00FFA342;
+    background: $subscribeButton;
+    border-radius: 6.6rem;
+    height: 4.9rem;
+    width: 16.8rem;
+}
+
+.subscribe-button span{
+    @include text(button);
+    color: $white;
+}
 
 
 </style>
